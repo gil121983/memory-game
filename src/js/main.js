15903338360d41d45849f19da1199df287f67ea0
gil121983 <gilstolar@icloud.com>
2020-02-15 04:58:26 +0200

    pink versio

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,17 @@
 init();
 
 function init() {
-  start();
+  sayHello()
 }
 
-function start() {
+function start(deck) {
   shuffle(deck);
   render(deck);
 }
 
-function render(cardsArray) {  
+function render(cardsArray) {
+  console.log(cardsArray);
+
   var gridElement = document.querySelector('.grid');
   const msgLine = document.querySelector('.message-line')
 
@@ -30,34 +32,66 @@ function render(cardsArray) {
   });
 
   let pickedCards = [];
+  let gameStatus = "on"
 
   function onCardClick(card, event) {
-    if (event.target.classList.contains('hidden') && card.id !== "empty-slot") {
+    if (event.target.classList.contains('hidden') && card.id !== "empty-slot" && gameStatus === "on") {
       if (pickedCards.length < 1) {
         event.target.classList.remove('hidden');
         pickedCards.push(card);
       } else {
         event.target.classList.remove('hidden');
         pickedCards.push(card);
-        if (pickedCards[0].key !== pickedCards[1].key){         
-          msgLine.innerHTML = `<h1>No Match</h1>`;
-          setTimeout(()=>flipBack(),3000);
-        }else{
-          msgLine.innerHTML = `<h1>It's a Match!</h1>`;
-          setTimeout(()=>removePair(pickedCards),3000);
-        }
+        if (pickedCards[0].key !== pickedCards[1].key)
+          flipBack();
+        else
+          removePair(pickedCards);
       }
-    } else {
-      msgLine.innerHTML = `<h1>Pick another card!</h1>`;
     }
   }
 
   function flipBack() {
-    clear()
-    render(deck)
+    gameStatus = "off"
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    Toast.fire({
+      icon: 'error',
+      title: ''
+    })
+    setTimeout(() => {
+      clear()
+      render(cardsArray)
+    }, 3000);
   }
 
-  function removePair(cards) {
+  function removePair(cards) { //remove matched pair and calls checkForWinner 
+    gameStatus = "off";
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 1000,
+      timerProgressBar: true,
+      onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    Toast.fire({
+      icon: 'success',
+    })
+
     let cardAindex = cardsArray.findIndex(item =>
       item.id === cards[0].id)
     cardsArray.splice(cardAindex, 1, emptySlot);
@@ -65,33 +99,92 @@ function render(cardsArray) {
     let cardBindex = cardsArray.findIndex(item =>
       item.id === cards[1].id)
     cardsArray.splice(cardBindex, 1, emptySlot);
-    
-    clear()
-    checkForWinner(cardsArray)
-    render(cardsArray)
+
+    setTimeout(() => {
+      clear()
+      checkForWinner(cardsArray)
+      render(cardsArray)
+    }, 1000);
   }
 
-  function checkForWinner(array){
+  function checkForWinner(array) { //declare victory and calls sayHello
     let count = 0;
 
     array.forEach(i => {
-      if(i.key)
-      count += 1;
-      if (count ===0){ // change it later to 2 and AUTO FLIP the last pair.
-        msgLine.innerHTML = `<h1>You Win !!!</h1>`;
-      }       
+      if (i.key)
+        count += 1;
+      if (count === 0) {
+        Swal.fire({
+          title: 'Oriki wins!!!.',
+          width: 600,
+          padding: '3em',
+          background: '#fff url(https://sweetalert2.github.io/images/trees.png)',
+          backdrop: `
+            rgba(0,0,123,0.4)
+            url(https://sweetalert2.github.io/images/nyan-cat.gif)
+            left top
+            no-repeat
+          `,
+          onClose() {
+            sayHello()
+          }
+        })
+      }
     })
 
-    
+
   }
-  
-  function clear() {
+
+  function clear() { //cleans the grid  
     while (gridElement.firstChild)
       gridElement.removeChild(gridElement.firstChild)
+    msgLine.innerHTML = ""
   }
 }
 
-function shuffle(array) {
+function sayHello() {  //says welcome and calls setDifficulty
+  Swal.fire({
+    title: `Welcome to Ori's memory game`,
+    confirmButtonText: 'New Game',
+    width: 600,
+    padding: '3em',
+    background: '#fff url(https://sweetalert2.github.io/images/trees.png)',
+    backdrop: `
+    rgba(0,0,123,0.4)
+    url(https://sweetalert2.github.io/images/nyan-cat.gif)
+    left top
+    no-repeat
+    `,
+    onClose() {
+      setDifficulty()
+    }
+  })
+}
+
+function setDifficulty() { //sets difficulty and calls start with new deck
+  Swal.fire({
+    title: 'Set diffuclty',
+    icon: 'question',
+    background: '#fff url(https://sweetalert2.github.io/images/trees.png)',
+    input: 'range',
+    inputAttributes: {
+      min: 4,
+      max: 18,
+      step: 2
+    },
+    inputValue: 10,
+    onClose() {
+      let diffuclty = Swal.getInput().value;
+      const newDeck = [];
+      for (let index = 0; index < diffuclty; index++) {
+        newDeck.push(allCards[index])
+      }
+      start(newDeck)
+    }
+  })
+}
+
+function shuffle(array) { // shuffle array randomly
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   // While there remain elements to shuffle...
@@ -106,6 +199,7 @@ function shuffle(array) {
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-  
+
   return array;
 }
+
